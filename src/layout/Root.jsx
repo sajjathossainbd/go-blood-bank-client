5ea3b19031deae5eb88269bd6fdcd40d868e73f6
sajjadsadiq <sajjadsadiq2020@gmail.com>
2024-06-08 21:51:27 +0600

    footer section desing completed

--- src/layout/Root.jsx
+++ src/layout/Root.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../pages/shared/Navbar";
+import Footer from "../pages/shared/Footer";
 
 function Root() {
   return (
     <div className="container mx-auto">
       <Navbar />
       <Outlet />
+      <Footer />
     </div>
   );
 }
